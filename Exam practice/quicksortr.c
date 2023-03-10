17a4ed631d8986193a36950e8261d2a15963933e
nishantcoder <nishantofficial2019@gmail.com>
2023-03-10 15:58:29 +0530

    quicksortr.c

--- Exam practice/quicksortr.c
+++ Exam practice/quicksortr.c
@@ -17,28 +17,25 @@ int part(int arr[], int low, int up)
         while (arr[end] > pivot)
             end--;
         if (start < end)
-            swap(&arr[start],&arr[end]);
-        
+            swap(&arr[start], &arr[end]);
     }
-    swap(&arr[low],&arr[end]);
+    swap(&arr[low], &arr[end]);
     return end;
 }
-int quicksort(int arr[],int low,int up)
+int quicksort(int arr[], int low, int up)
 {
-    if (low<up)
+    if (low < up)
     {
-        int lock=part(arr,low,up);
-        quicksort(arr,lock+1,up);
-        quicksort(arr,low,lock-1);
+        int lock = part(arr, low, up);
+        quicksort(arr, lock + 1, up);
+        quicksort(arr, low, lock - 1);
     }
 }
 int main()
 {
     int i, n, j, temp;
     printf("Enter Your no of elements in an array :\t");
-
     scanf("%d", &n);
-
     int arr[n];
 
     for (i = 0; i < n; i++)
@@ -46,7 +43,7 @@ int main()
         printf("...>");
         scanf("%d", &arr[i]);
     }
-    quicksort(arr,0,n-1);
+    quicksort(arr, 0, n - 1);
     printf("Sorted array is ");
     for (i = 0; i < n; i++)
     {
