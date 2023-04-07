1b98a2e4d6f7617035ad13fc981b0c69e841bc7f
nishantcoder <nishantofficial2019@gmail.com>
2023-04-07 17:50:06 +0530

    minmaxarraay

--- suhani/stringreverse.c
+++ suhani/stringreverse.c
@@ -5,7 +5,7 @@ int main()
     int len,j,count=0;
     printf("enter the string to be reversed\n");
     scanf("%s",str1);
-    for(len=0;str1[len]!=0;len++);
+    for(len=0;str1[len]!=0;len++); 
     for(j=len-1;j>=0;j--)
     {
         str2[count]=str1[j];
