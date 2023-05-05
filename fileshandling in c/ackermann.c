3cb9b76f3a877bc82f4352cd7af79b63386cdcb6
nishantcoder <nishantofficial2019@gmail.com>
2023-05-05 16:57:27 +0530

    ackermann

--- fileshandling in c/ackermann.c
+++ fileshandling in c/ackermann.c
@@ -9,10 +9,10 @@ int main()
 }
 int Ack(int m, int n)
 {
-        if(m==0)
-            return n+1;
-        else if(n==0)
-            return Ack(m-1,1);
-        else
-            return Ack(m-1,Ack(m,n-1));
+    if(m==0)
+        return n+1;
+    else if(n==0)
+        return Ack(m-1,1);
+    else
+        return Ack(m-1,Ack(m,n-1));
 }
