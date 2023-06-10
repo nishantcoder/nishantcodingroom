fc50f41af4a3c650789e17157f4282e9cf06c2e1
nishantcoder <nishantofficial2019@gmail.com>
2023-06-10 15:19:22 +0530

    Basics Of OOPS

---
+++ OOPS/BASICS OF OOPS/Inputoutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value for a ";
+    cin>>a;
+    cout<<"Value of a is "<<a;
+    return 0;
+}
---
+++ OOPS/BASICS OF OOPS/aithmaticopr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=56,b=90;
+    cout<<"The sum of a and b is "<<a+b<<"\nThe difference of a and b is "<<a-b;
+    cout<<"\nThe product of a and b is "<<a*b<<"\nThe division of a and b is "<<b/a;
+    cout<<"\nThe modulus of a and b is "<<b%a;
+    return 0;
+}
