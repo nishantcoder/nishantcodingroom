d24bf53697346b6b0a6831e5473de026ba5caabd
nishantcoder <nishantofficial2019@gmail.com>
2023-12-14 10:51:47 +0530

    bubble sort in bpmce

--- OOPS/College Coding OOPS/fact.cpp
+++ OOPS/College Coding OOPS/fact.cpp
@@ -1,20 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int fact(int n)
 {
-    if (n==0||n==1)
-      {
-        return 1;
-      }
+  if (n == 0 || n == 1)
+  {
+    return 1;
+  }
 
-      else
-      return n*fact(n-1);
+  else
+    return n * fact(n - 1);
 }
- int main()
-    {
-       cout<< fact(3);
-        return 0;
-
-    }
- 
+int main()
+{
+  cout << fact(3);
+  return 0;
+}
