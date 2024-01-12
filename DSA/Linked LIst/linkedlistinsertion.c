2eb86507c3a31d2a2d1291b93d66be173a442eac
nishantcoder <nishantofficial2019@gmail.com>
2024-01-12 22:33:41 +0530

    Linked list insertion and deletion

--- DSA/Linked LIst/linkedlistinsertion.c
+++ DSA/Linked LIst/linkedlistinsertion.c
@@ -5,17 +5,17 @@ struct node
 {
     int data;
     struct node *next;
-}*head=0;
+} *head = 0;
 
 int getlength()
 {
-    int i=0;
+    int i = 0;
     struct node *temp;
-    temp=head;
-    while (temp!=0)
+    temp = head;
+    while (temp != 0)
     {
         i++;
-        temp=temp->next;
+        temp = temp->next;
     }
     return i;
 }
@@ -23,59 +23,111 @@ int getlength()
 void insert_beg(int x)
 {
     struct node *newnode;
-    newnode=(struct node *)malloc(sizeof(struct node));
-    newnode->data=x;
-    newnode->next=head;
-    head=newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->next = head;
+    head = newnode;
 }
 
 void insert_end(int x)
 {
-    struct node *newnode,*temp;
-    newnode=(struct node *)malloc(sizeof(struct node));
-    newnode->data=x;
-    newnode->next=0;
-    while (temp->next!=0)
+    struct node *newnode, *temp;
+    temp = head;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->next = 0;
+    while (temp->next != 0)
     {
-        temp=temp->next;
+        temp = temp->next;
     }
-    temp->next=newnode;
+    temp->next = newnode;
 }
 
-void insert_pos(int pos,int x)
+void insert_pos(int pos, int x)
 {
-    struct node *newnode,*temp;
-    newnode=(struct node *)malloc(sizeof(struct node));
-    int count=getlength();
-    int i=1;
-    if (pos>count)
+    struct node *newnode, *temp;
+    temp = head;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    int count = getlength();
+    int i = 1;
+    if (pos > count)
     {
         printf("invalid position\n");
     }
     else
     {
-        temp=head;
-        while (i<pos)
+        temp = head;
+        while (i < pos)
+        {
+            temp = temp->next;
+            i++;
+        }
+        newnode->data = x;
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+}
+
+void delete_beg()
+{
+    struct node *temp;
+    temp = head;
+    head = temp->next;
+    free(temp);
+}
+
+void delete_end()
+{
+    struct node *temp, *prev;
+    temp = head;
+    while (temp->next != 0)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == head)
+    {
+        head = 0;
+    }
+    else
+    {
+        prev->next = 0;
+    }
+    free(temp);
+}
+
+void delete_pos(int pos)
+{
+    struct node *temp,*nextnode;
+    temp=head;
+    int len=getlength();
+    int i=0;
+    if (pos>len)
+    {
+        printf("Invalid Position\n");
+    }
+    else
+    {
+        while (i<pos-1)
         {
             temp=temp->next;
             i++;
         }
-        newnode->data=x;
-        newnode->next=temp->next;
-        temp->next=newnode;
-    }
-    
-    
+        nextnode=temp->next;
+        temp->next=nextnode->next;
+        free(nextnode);
+    } 
 }
+
 
 void display()
 {
     struct node *temp;
-    temp=head;
-    while (temp!=0)
+    temp = head;
+    while (temp != 0)
     {
-        printf("%d  ",temp->data);
-        temp=temp->next;
+        printf("%d  ", temp->data);
+        temp = temp->next;
     }
     printf("\n");
 }
@@ -85,12 +137,43 @@ int main()
     insert_beg(8);
     insert_beg(5);
     insert_beg(6);
+    printf("After insertion at beginning \t");
+    display();
 
     insert_end(11);
     insert_end(56);
+    printf("After insertion at End \t");
+    display();
 
-    insert_pos(3,78);
-    insert_pos(5,89);
+    insert_pos(3, 78);
+    printf("After insertion after position 3 \t");
+    display();
+    printf("After insertion after position 5 \t");
+    insert_pos(5, 89);
+    display();
+
+    printf("After deleting from beginning\t");
+    delete_beg();
+    display();
+
+    printf("After deleting from end \t");
+    delete_end();
+    display();
+    
+    printf("After deleting from position 3rd \t\t");
+    delete_pos(3);
     display();
     return 0;
 }
+
+/*
+Output
+here position means the index
+After insertion at beginning    6  5  8
+After insertion at End  6  5  8  11  56
+After insertion after position 3        6  5  8  78  11  56
+After insertion after position 5        6  5  8  78  11  89  56
+After deleting from beginning   5  8  78  11  89  56  
+After deleting from end         5  8  78  11  89  
+After deleting from position 3rd                5  8  78  89  
+*/
